747be29be130470ca28b18561eb66b11d5c8effe
chick_0 <chick_0@ch1ck.xyz>
2022-07-30 14:37:06 +0900

    ask if name is empty

--- Assets/Scripts/StartScene.cs
+++ Assets/Scripts/StartScene.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 using TMPro;
 
 public class StartScene : MonoBehaviour
@@ -16,6 +17,21 @@ public class StartScene : MonoBehaviour
         {
             Director.Name = name;
             SceneManager.LoadScene("GameScene");
+        }
+        else
+        {
+            bool ask = EditorUtility.DisplayDialog(
+                "이름",
+                "게임을 시작하려면 이름을 입력해야 합니다.", 
+                "이름 입력하기",
+                "익명으로 계속하기"
+            );
+
+            if (ask == false)
+            {
+                Director.Name = "ㅇㅇ";
+                SceneManager.LoadScene("GameScene");
+            }
         }
     }
 
